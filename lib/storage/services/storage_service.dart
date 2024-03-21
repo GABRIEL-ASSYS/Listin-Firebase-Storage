ce24573181f9d3e4df35b5de17af0a742dea694d
Gabriel Assys <engs-gabrieloliveira@camporeal.edu.br>
2024-03-21 08:59:29 -0300

    criando storage_service.dart

---
+++ lib/storage/services/storage_service.dart
@@ -0,0 +1,13 @@
+import 'dart:ffi';
+import 'dart:io';
+
+import 'package:firebase_storage/firebase_storage.dart';
+
+class StorageService {
+  String pathService = "images";
+  final FirebaseStorage _firebaseStorage = FirebaseStorage.instance;
+
+  upload({required File file, required String fileName}) async {
+    await _firebaseStorage.ref("$pathService/$fileName.png").putFile(file);
+  }
+}
